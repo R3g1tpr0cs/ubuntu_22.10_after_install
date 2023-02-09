a73045832ec52509bcae37c28634aeb8d3a320a0
Vladimir Kreyson <vladimir@kreyson.ru>
2023-02-09 10:24:56 +0300

    Added new soft

--- ubuntu_after_install.sh
+++ ubuntu_after_install.sh
@@ -12,8 +12,11 @@ echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-st
 exit
 #client for eve-ng
 sudo add-apt-repository ppa:smartfinn/eve-ng-integration
-#curl
-sudo apt install curl -y
+#repo for Ansible
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+#curl and ansible
+sudo apt install curl ansible -y
 #repo for pgadmin
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
@@ -38,6 +41,10 @@ EOF
 #update and upgrade with new repos
 sudo apt update
 sudo apt dist-upgrade -y
+#install wireguard
+sudo apt install wireguard
+#Создать конфиг для wg в файле - sudo nano /etc/wireguard/wg0.conf
+#Запуск - sudo wg-quick up wg0
 #install wine
 sudo apt install --install-recommends winehq-stable
 wine winecfg
@@ -51,7 +58,7 @@ sudo apt install openjdk-17-jre-headless default-jre -y
 sudo apt install keepassxc chromium-browser vlc audacity gimp minicom -y
 sudo apt install obs-studio zeal eve-ng-integration steam-installer python3-pip python3-venv -y
 #install soft from snap
-sudo snap install discord telegram-desktop dbeaver-ce code --classic
+sudo snap install discord telegram-desktop fbreader dbeaver-ce code --classic
 #install kali repos
 sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list" -y
 sudo apt install gnupg -y
@@ -59,9 +66,26 @@ wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
 sudo apt update
 sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref" -y
+#install repo for vagrant
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 #update
 sudo apt update
 #install kali soft
 sudo aptitude install -t kali-rolling wpscan dirsearch gobuster airgeddon bettercap ettercap-graphical hostapd-wpe bully pixiewps dhcpd asleap hashcat hostapd tshark mdk4 hcxdumptool reaver john crunch beef lighttpd -y
-
-
+#install zsh, vagrant и oh-my-zsh
+sudo apt install zsh vagrant -y
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#Изменить шелл по-умолчанию
+chsh
+#Ввести /bin/bash
+#Настройка подсветки
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+mv zsh-syntax-highlighting ~/.oh-my-zsh/plugins
+#Затем вставьте следующую строку в конец файла ~/.zshrc: source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zshrc
+#Настроика автозаполнения на Zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions
+mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
+#Затем добавьте плагин в список плагинов в файле ~/.zshrc: plugins=(git docker gcloud terraform vagrant zsh-autosuggestions)
+source ~/.zshrc
